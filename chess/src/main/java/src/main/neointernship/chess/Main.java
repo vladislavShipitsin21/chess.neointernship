21113ec313becc1dfa3cbfb272918422aea9eb8a
Gaziz Sattarov <gaziz.sattarov@gmail.com>
2020-07-06 16:02:50 +0600

    basic structure has been added

--- chess/src/main/java/src/main/neointernship/chess/Main.java
+++ chess/src/main/java/src/main/neointernship/chess/Main.java
@@ -1,4 +1,15 @@
 package src.main.neointernship.chess;
 
+/**
+ * Гла
+ */
 public class Main {
+
+    /**
+     *
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) {
+
+    }
 }
