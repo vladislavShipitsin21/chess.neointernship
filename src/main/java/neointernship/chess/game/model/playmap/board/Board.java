6c525bf9174beb624e20b727c38b456fdca11bdf
Gaziz Sattarov <gaziz.sattarov@gmail.com>
2020-07-07 17:09:27 +0600

    board has been modified

--- src/main/java/neointernship/chess/game/model/playmap/board/Board.java
+++ src/main/java/neointernship/chess/game/model/playmap/board/Board.java
@@ -1,7 +1,47 @@
 package neointernship.chess.game.model.playmap.board;
 
-import java.lang.reflect.Field;
+import neointernship.chess.game.model.figure.Figure;
+import neointernship.chess.game.model.mediator.Mediator;
+import neointernship.chess.game.model.playmap.field.IField;
 
-public class Board {
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class Board implements IBoard {
+    private final Mediator mediator;
+
+    private final String chessType;
+
     private Field[][] fieldMatrix;
+
+    public Board(final String chessType) {
+        mediator = new Mediator();
+
+        this.chessType = chessType;
+    }
+
+    @Override
+    public void initializeBoard() {
+
+    }
+
+    @Override
+    public IField getField(final int x, final int y) {
+        return null;
+    }
+
+    @Override
+    public IField getField(final Figure figure) {
+        return null;
+    }
+
+    @Override
+    public Figure getFigure(final IField field) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Figure> getFigures() {
+        return null;
+    }
 }
